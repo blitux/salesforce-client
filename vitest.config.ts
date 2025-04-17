7dd1a1ee3bfbbd93073ac88b19b9b7abad02e712
Blitux <dev.pabloquiroga@gmail.com>
2025-04-17 17:19:33 -0300

    fix: use v8 for vitest coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,16 +1,21 @@
-import { defineConfig, configDefaults, coverageConfigDefaults } from 'vitest/config'
+import {
+  configDefaults,
+  coverageConfigDefaults,
+  defineConfig,
+} from "vitest/config";
 
 export default defineConfig({
   test: {
-    include: ['src/**/*.test.ts'],
+    include: ["src/**/*.test.ts"],
     coverage: {
       exclude: [
         ...coverageConfigDefaults.exclude,
-        'old/**',
-        'commitlint.config.js',
-        'node_modules'
+        "old/**",
+        "commitlint.config.js",
+        "node_modules",
       ],
-      provider: 'istanbul'
+      provider: "v8",
+      reporter: ["text", "html"],
     },
   },
-})
+});
